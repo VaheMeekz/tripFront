4757b1a51841f6056178e0a38dd9aa3eb03eb863
Vahe <71688163+VaheMeekz@users.noreply.github.com>
2022-06-08 13:41:26 +0400

    Add files via upload

--- src/utils/config/keys.js
+++ src/utils/config/keys.js
@@ -7,7 +7,7 @@
 
 export const baseUrl = 'https://trip003.herokuapp.com/api/v1'
 export const token = localStorage.getItem("token")
-export const myUrl = "http://localhost:3000"
+export const myUrl = "https://trip-front.vercel.app"
 export const headers = {
     'Authorization': `Bearer ${token}`
 }
